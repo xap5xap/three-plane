3e094e30b2964bba02cd37b22c392fc885c6706b
Xavier Perez <xap5xap@gmail.com>
2017-06-09 10:11:48 -0500

    sea

---
+++ js/game.js
@@ -0,0 +1,183 @@
+//COLORS
+var Colors = {
+    red: 0xf25346,
+    white: 0xd8d0d1,
+    brown: 0x59332e,
+    brownDark: 0x23190f,
+    pink: 0xF5986E,
+    yellow: 0xf4ce93,
+    blue: 0x68c3c0,
+
+};
+
+// var THREE;
+
+//THREEJS RELATED VARIABLES
+var scene,
+    camera, fieldOfView, aspectRatio, nearPlane, farPlane, HEIGHT, WIDTH,
+    renderer, container;
+
+//LIGHTS
+var hemisphereLight, shadowLight;
+
+function createScene() {
+    console.log('creo scena');
+    // Get the width and the height of the screen,
+    // use them to set up the aspect ratio of the camera 
+    // and the size of the renderer.
+    HEIGHT = window.innerHeight;
+    WIDTH = window.innerWidth;
+
+    // Create the scene
+    scene = new THREE.Scene();
+
+    // Add a fog effect to the scene; same color as the
+    // background color used in the style sheet
+    scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+
+    // Create the camera
+    aspectRatio = WIDTH / HEIGHT;
+    fieldOfView = 60;
+    nearPlane = 1;
+    farPlane = 10000;
+    camera = new THREE.PerspectiveCamera(
+        fieldOfView,
+        aspectRatio,
+        nearPlane,
+        farPlane
+    );
+
+    // Set the position of the camera
+    camera.position.x = 0;
+    camera.position.z = 200;
+    camera.position.y = 100;
+
+    // Create the renderer
+    renderer = new THREE.WebGLRenderer({
+        // Allow transparency to show the gradient background
+        // we defined in the CSS
+        alpha: true,
+
+        // Activate the anti-aliasing; this is less performant,
+        // but, as our project is low-poly based, it should be fine :)
+        antialias: true
+    });
+
+    // Define the size of the renderer; in this case,
+    // it will fill the entire screen
+    renderer.setSize(WIDTH, HEIGHT);
+
+    // Enable shadow rendering
+    renderer.shadowMap.enabled = true;
+
+    // Add the DOM element of the renderer to the 
+    // container we created in the HTML
+    container = document.getElementById('world');
+    container.appendChild(renderer.domElement);
+
+    // Listen to the screen: if the user resizes it
+    // we have to update the camera and the renderer size
+    window.addEventListener('resize', handleWindowResize, false);
+}
+
+function handleWindowResize() {
+    //update height and width of the renderer and the camera
+    HEIGHT = window.innerHeight;
+    WIDTH = window.innerWidth;
+    renderer.setSize(WIDTH, HEIGHT);
+    camera.aspect = WIDTH / HEIGHT;
+    camera.updateProjectionMatrix();
+}
+
+function createLights() {
+    //A hemsphere light is a gradient colored light
+    //the first parameter is the sky color, the second parameter is the groudn color,
+    //the third parameter is the intensity of the light
+    hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, .9);
+
+    //A directional light shines from a specific direction
+    //It acts like the sun, that means that all the rays produced are paralel
+    shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+
+    //set the direction of the light
+    shadowLight.position.set(150, 350, 350);
+
+    //Allow shadow casting
+    shadowLight.castShadow = true;
+
+    //Define the visible area of the projected shadow
+    shadowLight.shadow.camera.left = -400;
+    shadowLight.shadow.camera.right = 400;
+    shadowLight.shadow.camera.top = 400;
+    shadowLight.shadow.camera.bottom = -400;
+    shadowLight.shadow.camera.near = 1;
+    shadowLight.shadow.camera.far = 1000;
+
+    //Define the resolution of the shadow; the higher the better
+    //but also the more expensive and less performant
+    shadowLight.shadow.mapSize.width = 2048;
+    shadowLight.shadow.mapSize.height = 2048;
+
+    //to activae the lights. just add them to the scene
+    scene.add(hemisphereLight);
+    scene.add(shadowLight);
+}
+
+//first defibe a Sea object
+Sea = function () {
+    //create the geometry (shape) of the cylinder
+    //the parameters are radius top, radius botom, height, number of segments on the radious, number of segments
+    var geom = new THREE.CylinderGeometry(600, 600, 800, 40, 10);
+
+    //rotate the geometry on the x axis
+    geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+
+    //create the material
+    var mat = new THREE.MeshPhongMaterial({
+        color: Colors.blue,
+        transparent: true,
+        opacity: .6,
+        shading: THREE.FlatShading
+    });
+
+    //To create an object in three we have to create a mesh
+    //wich is a cpmbination of a geometry and some material
+    this.mesh = new THREE.Mesh(geom, mat);
+    //Allow the sea to receive shadows
+    this.mesh.receiveShadow = true;
+}
+
+var sea;
+
+function createSea() {
+    sea = new Sea();
+
+    //push it a little bit at bottom of the screen
+    sea.mesh.position.y = -600;
+
+    //add the mesh of the sea to the scene
+    scene.add(sea.mesh);
+}
+
+function init() {
+
+    // set up the scene, the camera and the renderer
+    createScene();
+
+    // add the lights
+    createLights();
+
+    // add the objects
+    createSea();
+    /*
+  createPlane();
+  
+  createSky();
+ 
+  // start a loop that will update the objects' positions 
+  // and render the scene on each frame
+  loop();
+  */
+}
+
+window.addEventListener('load', init, false);
